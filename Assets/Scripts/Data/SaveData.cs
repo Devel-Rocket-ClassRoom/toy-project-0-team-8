0a36c0af4e1af0b460aa81ec455569803103ee90
hgy4152 <hgy4152@gmail.com>
2026-05-04 00:27:24 +0900

    Data
    데이터 저장을 위한 스크립트 정리

---
+++ Assets/Scripts/Data/SaveData.cs
@@ -0,0 +1,30 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEditor.Overlays;
+using UnityEngine;
+using static UnityEditor.Progress;
+using static UnityEngine.LowLevelPhysics2D.PhysicsLayers;
+
+[System.Serializable]
+public abstract class SaveData
+{
+    public int Version { get; protected set; }
+    public abstract SaveData VersionUp();
+}
+
+
+[System.Serializable]
+public class SaveDataV1 : SaveData
+{
+    public string PlayerName {  get; set; } = string.Empty;
+    public SaveDataV1()
+    {
+        Version = 1;
+    }
+
+    public override SaveData VersionUp()
+    {
+        return null;
+    }
+}
+
