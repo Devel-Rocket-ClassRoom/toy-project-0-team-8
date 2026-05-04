64480d553a67b53ad848756090c5b3363c8d4747
남상준 <skatkdwns08@gmail.com>
2026-05-04 09:23:37 +0900

    크리스탈부족시 가운데에 텍스트로 띄운후 점점줄어들게 함

--- Assets/Scripts/Ui/Gacha/GachaTitle.cs
+++ Assets/Scripts/Ui/Gacha/GachaTitle.cs
@@ -1,19 +1,24 @@
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.UI;   
 using TMPro;
 using System;
+using System.Collections;
 public class GachaTitle : GenericWindow
 {
     public long currentCrystal;
     private long textcurrentCrystal;
     public TextMeshProUGUI crystaltext;
     public GameObject addcrystalPanal;
+    public GameObject enoughCrystalPanal;
+    public TextMeshProUGUI enoughCrystalText;
     public long maxCrystal = 99999999999;
+    private bool notenoughCheck;
     private int GachaCount;
 
     private void Awake()
     {
         textcurrentCrystal = currentCrystal;
+        enoughCrystalPanal.SetActive(false);
         crystaltext.text = textcurrentCrystal.ToString();
     }
     public void OnClickAddCrystal(int crystal)
@@ -33,10 +38,12 @@ public class GachaTitle : GenericWindow
     public override void Open()
     {
         base.Open();
+        enoughCrystalPanal.SetActive(false);
     }
     public override void Close()
     {
         base.Close();
+        enoughCrystalPanal.SetActive(false);
     }
 
     public void OnOneClickGachaButton()
@@ -44,6 +51,7 @@ public class GachaTitle : GenericWindow
         if (currentCrystal < 200)
         {
             Debug.Log("크리스탈 부족");
+            StartCoroutine(NotEnoughCrystal());
             return;
         }
         currentCrystal -= 200;
@@ -57,6 +65,9 @@ public class GachaTitle : GenericWindow
         if (currentCrystal < 2000)
         {
             Debug.Log("크리스탈 부족");
+            if (notenoughCheck) return;
+
+            StartCoroutine(NotEnoughCrystal());
             return;
         }
         currentCrystal -= 2000;
@@ -64,5 +75,24 @@ public class GachaTitle : GenericWindow
         textcurrentCrystal = Math.Clamp(currentCrystal, 0, maxCrystal);
         crystaltext.text = $"{textcurrentCrystal.ToString()}";
         windowManager.Open(1,GachaCount);
+    }
+    public IEnumerator NotEnoughCrystal()
+    {
+        enoughCrystalPanal.SetActive(false);
+        enoughCrystalPanal.SetActive(true);
+        notenoughCheck = true;
+        float t = 0;
+        while(t<1)
+        {
+            t += Time.deltaTime;
+            enoughCrystalPanal.transform.localScale = Vector3.Lerp(Vector3.one, new Vector3(0.1f, 0.1f, 0.1f),t);
+            if(Time.frameCount%5==0)
+            {
+                enoughCrystalText.color = UnityEngine.Random.ColorHSV(0f, 1f, 0.5f, 1f, 0.7f, 1f);
+            }
+            yield return null;
+        }
+        notenoughCheck = false;
+        enoughCrystalPanal.SetActive(false);
     }
 }
