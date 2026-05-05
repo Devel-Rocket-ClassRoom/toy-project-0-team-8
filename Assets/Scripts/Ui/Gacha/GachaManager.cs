382e8256ad5351729d66f58bb2d9934e79d06ee5
hgy4152 <hgy4152@gmail.com>
2026-05-05 21:28:20 +0900

    Scriptable Object Issue
    한 스크립트에 다 뭉쳐놔서 오류 발생
    파일 분리해서 해결 완료.

--- Assets/Scripts/Ui/Gacha/GachaManager.cs
+++ Assets/Scripts/Ui/Gacha/GachaManager.cs
@@ -3,28 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 
-
-[CreateAssetMenu(fileName = "GachaGear", menuName = "Gacha/Gear")]
-public class GachaGear : ScriptableObject
-{
-    public string itemName;
-    public string itemId;
-    public float weight;
-    public Sprite Icon;
-}
-
-[CreateAssetMenu(fileName = "GachaCookie", menuName = "Gacha/Cookie")]
-public class GachaCookie : ScriptableObject
-{
-    public string cookieName;
-    public string cookieId;
-    public float weight;
-    public Sprite Icon;
-
-    // 스킬 스크립트도 여기 추가해서 써도 됌
-}
-
-
 public class GachaManager : MonoBehaviour
 {
     private float totalWeight;
