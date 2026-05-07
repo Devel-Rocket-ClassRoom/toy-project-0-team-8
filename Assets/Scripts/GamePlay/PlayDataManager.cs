6097c1efadd2983e2c7f13eff998e896cbc7f49b
정호종 <33508056+LteFroggy@users.noreply.github.com>
2026-05-07 17:21:07 +0900

    feature(#59) : 사용되는 파일 명 변경

---
+++ Assets/Scripts/GamePlay/PlayDataManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayDataManager : MonoBehaviour {
+	public static PlayDataManager Instance;
+	
+	public CookieData CookieData;
+	public int EarnedCoin;
+	public int Score;
+	public int StageIdx;
+	public bool IsMaxScoreRenewed;
+
+	private void Awake() {
+		if (Instance != null) {
+			Destroy(gameObject);
+			return;
+		}
+		
+		Instance = this;
+		DontDestroyOnLoad(gameObject);
+	}
+}
