c4f03b660e55712163ff1d45dc833f446ccbfc99
정호종 <33508056+LteFroggy@users.noreply.github.com>
2026-05-08 13:00:30 +0900

    feature(#122) : 로비에서 게임 종료 버튼 구현

---
+++ Assets/Scripts/Ui/Lobby/MenuPanel.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class MenuPanel : MonoBehaviour {
+	[Header("=== 계속하기 버튼 ===")]
+	[SerializeField] private Button _resumeButton;
+	[Header("=== 종료 버튼 ===")]
+	[SerializeField] private Button _quitButton;
+	
+	// 종료 버튼에 붙을 이벤트
+	[HideInInspector] public UnityEvent OnQuitButtonPressed;
+	[HideInInspector] public UnityEvent OnResumeButtonPressed;
+	
+	private void Awake() {
+		// 이벤트 붙이기
+		_quitButton.onClick.AddListener(() => OnQuitButtonPressed?.Invoke());
+		_resumeButton.onClick.AddListener(() => OnResumeButtonPressed?.Invoke());
+	}
+}
