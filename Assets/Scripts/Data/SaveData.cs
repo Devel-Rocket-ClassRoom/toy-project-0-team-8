1f2d63a66be8d30dfee784f56b55b3263cf986dd
정호종 <33508056+LteFroggy@users.noreply.github.com>
2026-05-08 11:44:34 +0900

    cleanup : 불필요한 Import문 제거

--- Assets/Scripts/Data/SaveData.cs
+++ Assets/Scripts/Data/SaveData.cs
@@ -1,12 +1,7 @@
-using NUnit.Framework;
+using System;
 using System.Collections.Generic;
-using UnityEditor.Overlays;
-using UnityEngine;
-using UnityEngine.UI;
-using static UnityEditor.Progress;
-using static UnityEngine.LowLevelPhysics2D.PhysicsLayers;
 
-[System.Serializable]
+[Serializable]
 public abstract class SaveData
 {
     public int Version { get; protected set; }
@@ -14,7 +9,7 @@ public abstract class SaveData
 }
 
 
-[System.Serializable]
+[Serializable]
 public class SaveDataV1 : SaveData
 {
     // 저장할 때 필요한 데이터
